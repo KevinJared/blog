243d4c41d3d23d9e99d55d709108a436a62b4ca3
kevinjared <kevinjaredkip@gmail.com>
2018-09-16 17:39:13 +0300

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from flask_simplemde import SimpleMDE
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = False
 db = SQLAlchemy(app)
 bootstrap = Bootstrap()
 mail = Mail()
