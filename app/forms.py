4cc3b5d40edec7976dc36e80a45930f219253544
kevinjared <kevinjaredkip@gmail.com>
2018-09-16 20:01:28 +0300

    styling

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+class PostForm(FlaskForm):
+    category = SelectField('Category', choices=[('Technology','Technology'),('Pickuplines','Pickuplines'),('BusinessPitch','Business Pitch'),('Sales','Sales'),('Interview','Interview')])
+    post = TextAreaField(('Say something'), validators=[Required()])
+    submit = SubmitField(('Submit'))
 
-    title = StringField('comment title',validators=[Required()])
-    review = TextAreaField('comment review', validators=[Required()])
-    submit = SubmitField('Submit')
+class CommentForm(FlaskForm):
+    details = StringField('Write a comment',validators=[Required()])
+    submit = SubmitField('Comment')
